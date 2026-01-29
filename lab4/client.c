10dc330485cfdf17a86fde699e00505710c8f63a
Riya Shanbhag <148786781+riyashanbhag@users.noreply.github.com>
2026-01-29 10:10:01 +0530

    Refactor client.c for clarity and error handling

--- lab4/client.c
+++ lab4/client.c
@@ -4,45 +4,50 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
 
 #define PORT 8080
 
 int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-    char message[1024];
+    int sock;
+    struct sockaddr_in server_addr;
     char buffer[1024] = {0};
-
-    // Create socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    // Connect to server
-    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-
+    char message[1024];
     int option;
     char input[100];
 
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket error");
+        exit(1);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(sock, (struct sockaddr *)&server_addr,
+                sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        exit(1);
+    }
+
+    printf("\n--- CLIENT MENU ---\n");
     printf("1. Registration Number\n");
     printf("2. Name of Student\n");
     printf("3. Subject Code\n");
-    printf("Enter your choice: ");
+    printf("Enter option: ");
     scanf("%d", &option);
-    getchar();  // consume leftover newline from scanf
+    getchar();
 
-    printf("Enter data: ");
+    printf("Enter value: ");
     fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = 0;  // remove trailing newline
+    input[strcspn(input, "\n")] = 0;
 
     sprintf(message, "%d %s", option, input);
     send(sock, message, strlen(message), 0);
 
-    read(sock, buffer, sizeof(buffer));
-    printf("\n--- Server Response ---\n%s\n", buffer);
+    recv(sock, buffer, sizeof(buffer), 0);
+    printf("\n--- SERVER RESPONSE ---\n%s\n", buffer);
 
     close(sock);
     return 0;
