e40ddc22c67d46b582d8fc2a6e1c9c1700cac10f
Riya Shanbhag <148786781+riyashanbhag@users.noreply.github.com>
2026-02-05 09:18:22 +0530

    added q1 client 1 and client 2 , data.txt files

---
+++ lab5/client1.c
@@ -0,0 +1,25 @@
+// client1.c
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+    int sock;
+    struct sockaddr_in server;
+    char msg[] = "Institute Of";
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
+
+    connect(sock, (struct sockaddr *)&server, sizeof(server));
+
+    send(sock, msg, strlen(msg), 0);
+    close(sock);
+    return 0;
+}
